960270783da9ad538d87c93eaead07300b8fa610
Nicolas Ledez <github@ledez.net>
2012-11-18 14:59:58 +0100

    Initial Commit

---
+++ MailTesterOSX/AppDelegate.rb
@@ -0,0 +1,15 @@
+#
+#  AppDelegate.rb
+#  MailTesterOSX
+#
+#  Created by Nicolas on 18/11/12.
+#  Copyright 2012 Nicolas. All rights reserved.
+#
+
+class AppDelegate
+    attr_accessor :window
+    def applicationDidFinishLaunching(a_notification)
+        # Insert code here to initialize your application
+    end
+end
+
